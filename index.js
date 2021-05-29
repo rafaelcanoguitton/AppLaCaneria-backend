db1659c09a953fca1ee83635b24faec86bca55b7
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-05-28 22:14:55 -0500

    Attempting to fix server

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const session = require('express-session');
+const redisStore = require('connect-redis')(session);
 const app = express();
 //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: true, store: new RedisStore()}));
+app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: true, store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  260})}));
 //routes
 app.use(require('./routes/index'));
 const port = process.env.PORT || config.httpPort;
