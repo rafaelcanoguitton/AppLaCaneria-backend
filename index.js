b928b652e118456566e8b11d8dfdbd7c0fb4a47a
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-05-29 08:01:31 -0500

    Attempting to fix server

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const session = require('express-session');
-const app = express()
+const express = require("express");
+const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
+const app = express();
 //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: true}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore({
+        checkPeriod: 86400000,
+    })
+  })
+);
 //routes
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 const port = process.env.PORT || config.httpPort;
 app.listen(port, () => {
-    console.log("App is running on port: " + port);
+  console.log("App is running on port: " + port);
 });
 module.exports = app;
